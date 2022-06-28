47120f15204027609041ccfe3f184fa52d36bfda
DeEzUzZa-7012 <108201261+DeEzUzZa-7012@users.noreply.github.com>
2022-06-28 09:50:52 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -91,8 +91,7 @@ async def cb_navg(bot, update: CallbackQuery):
         temp_results.append([
             InlineKeyboardButton("⏪ Back", callback_data=f"navigate({index_val}|back|{query})"),
             InlineKeyboardButton(f"🔰 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🔰", callback_data="ignore")
-            InlineKeyboardButton("Next ⏩", callback_data=f"navigate({index_val}|next|{query})")
-        ]')
+            InlineKeyboardButton("Next ⏩", callback_data=f"navigate({index_val}|next|{query}")])
 
     if not int(index_val) == 0:    
         temp_results.append([
